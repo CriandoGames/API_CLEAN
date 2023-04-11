56e04f319b3bd4f4b6fddef8dbe9926befc5bd1d
Carlos Castro <carloscastrogames@gmail.com>
2023-04-11 15:50:21 -0300

    ➕ : pacote FluentValidation

---
+++ Manager.Domain/Entities/Base.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Manager.Domain.Entities
+{
+    public abstract class Base
+    {
+
+        public Base() {
+            _errors = new List<string>();
+            id = Guid.NewGuid();
+            CreateAt = DateTime.Now; 
+        }
+
+
+      public Guid id { get; set; }
+      public DateTime CreateAt { get; set; }
+
+      internal List<string> _errors;
+      public IReadOnlyCollection<string> Erros => _errors;
+
+      public abstract bool Validate();
+
+    }
+}
---
+++ Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Manager.Domain.Entities;
+
+namespace Manager.Domain.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x).NotEmpty()
+                   .WithMessage("A entidade não pode ser vazia").
+                   NotNull().WithMessage("A entidade não poser ser nula");    
+
+            RuleFor(x => x.Name).NotEmpty()
+                    .WithMessage("O nome não pode ser vazio").
+                    NotNull().WithMessage("O nome não poser ser nula").
+                    MinimumLength(3).WithMessage("O nome deve ter no Mínimo 3 caracteres").
+                    MaximumLength(80).WithMessage("Onome deve ter no maximo 80 caracteres");
+
+
+            RuleFor(x => x.Email)
+                    .NotEmpty().WithMessage("Informe o e-mail do cliente")
+                    .EmailAddress().WithMessage("E-mail inválido");
+
+
+            RuleFor(x => x.Password).NotEmpty()
+                  .WithMessage("A senha não pode ser vazio").
+                  NotNull().WithMessage("A senha não poser ser nula").
+                  MinimumLength(6).WithMessage("A senha deve ter no Mínimo 6 caracteres").
+                  MaximumLength(80).WithMessage("A senha deve ter no maximo 80 caracteres");
+
+
+        }
+    }
+}
